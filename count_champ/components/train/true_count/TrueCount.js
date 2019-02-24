b78f27b6f1caae58820f5094506017b47def5806
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-02-23 23:41:57 -0700

    added true count conversion component skelleton

--- count_champ/components/train/true_count/TrueCount.js
+++ count_champ/components/train/true_count/TrueCount.js
@@ -1,10 +1,71 @@
 import * as React from 'react';
-import { Button, Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import { Button, Text, View, StyleSheet, Image, TextInput, Dimensions } from 'react-native';
 import { Constants } from 'expo';
 
 let ScreenHeight = Dimensions.get("window").height;
 
 class TrueCount extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            randomDeck: 2,
+            randomCount: 2,
+            guess: 1,
+            answer: 1,
+            wereTheyRight: true
+        }
+    }
+
+    clickNew = () => {
+        console.log('guess is : ' + this.state.guess)
+        // this.setState({
+        //     wereTheyRight: true
+        // })
+        this.generateNumbers()
+    }
+    generateNumbers = () => {
+        const decksLeftArray = [1,2,3,4,5,6,7,8];
+        let randomDeck = decksLeftArray[Math.floor(Math.random()*decksLeftArray.length)]
+        const countArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        let randomCount = countArray[Math.floor(Math.random()*countArray.length)]
+        this.setState({
+            randomDeck: randomDeck,
+            randomCount: randomCount
+        })
+    }
+
+    handleChange = event => {
+        this.setState({ 
+            [event.target.name]: event.target.value,  
+        }) 
+    }
+
+    clickCheck = () => {
+        console.log('check was clicked')
+        const deck = this.state.randomDeck;
+        const count = this.state.randomCount;
+        const correctAnswer = (Math.round( count / deck));
+        this.setState({
+            answer: correctAnswer,
+        }, () => this.checkAnswer() )
+    }
+
+    checkAnswer = () => {
+        let guessNumber = parseInt(this.state.guess)
+        if (this.state.answer === guessNumber){
+            this.setState({
+                wereTheyRight: true,
+            })
+            return <h2>Correct!</h2>
+        } else{
+            this.setState({
+                wereTheyRight: false
+            })
+            return <h2> answered wrong</h2>
+        }
+    }
+    
+
     static navigationOptions = {
         title: 'Count Champ',
     };
@@ -13,36 +74,34 @@ class TrueCount extends React.Component {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
-                <Text>
-                    True Count Drill
-                </Text>
-                {/* <View style={styles.buttonContainer}>
-                    <Button
-                    title="Basic Strategy"
-                    color="#000000"
-                    onPress={() => navigate('Home', {name: 'Basic Strategy'})}
+                <View className='container'>
+                    <Text className='trainDrillSubtitle'>Convert to True Count Drill</Text>
+                    <Text className='subTitle'>Running Count is: {this.state.randomCount} </Text>
+                    <Text className='subTitle'>Decks Left: {this.state.randomDeck}</Text>
+                    <Text className='subTitleMargin'>What's the true count? (round) </Text>
+                    <TextInput
+                        style={{height: 40, borderColor: 'black', borderWidth: 1, borderRadius: 10, backgroundColor: 'white', opacity: 0.7, width: 100, paddingTop: 5, paddingBottom: 5, paddingLeft: 35, fontSize: 26, fontWeight: 'bold'}}
+                        keyboardType = 'phone-pad'
+                        maxLength={2}
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.text}
                     />
-                    <Button
-                    title="Self Paced Count"
-                    color="#000000"
-                    onPress={() => navigate('Learn', {name: 'Self Paced Count'})}
-                    />
-                    <Button
-                    title="Speed Count"
-                    color="#000000"
-                    onPress={() => navigate('Train', {name: 'Speed Count'})}
-                    />
-                    <Button
-                    title="True Count"
-                    color="#000000"
-                    onPress={() => navigate('Train', {name: 'True Count'})}
-                    />
-                    <Button
-                    title="Bet Sizing"
-                    color="#000000"
-                    onPress={() => navigate('Train', {name: 'Bet Sizing'})}
-                    />
-                </View> */}
+                    {/* <form className='runningCountTrueCountForm'>
+                        <input  className='runCountTrueCountInput' 
+                                type='number' 
+                                name='guess' 
+                                value={this.state.guess} 
+                                onChange={this.handleChange} 
+                                placeholder='Enter True Count' >
+                        </input>
+                    </form> */}
+                    <View style={styles.buttonContainer}>
+                        <Button color="#000000" onPress={this.clickNew} title="New"></Button>
+                        <Button color="#000000" onPress={this.clickCheck} title="Check"></Button>
+                    </View>
+                    <Text className={this.state.wereTheyRight ? 'showCountDiv' : 'hideCountDiv' }>Correct!</Text>
+                    <Text className={this.state.wereTheyRight ? 'hideCountDiv' : 'showCountDiv' }>The Answer Was: {this.state.answer}</Text>
+                </View>
         </View>
 
 
@@ -59,11 +118,11 @@ const styles = StyleSheet.create({
         height: ScreenHeight,
     },
     buttonContainer: {
-        marginTop: 100,
+        marginTop: 0,
         flex: 0,
         justifyContent: 'space-evenly',
-        height: 350,
+        height: 120,
     },
-});
+});                                    
 
 export default TrueCount
