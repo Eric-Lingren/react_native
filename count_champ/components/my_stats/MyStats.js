f5211c279041e3a5accd9a66a830ea72025499be
Eric-Lingren <ericlingren@gmail.com>
2019-03-01 14:49:31 -0700

    total hands stats functional and styled

--- count_champ/components/my_stats/MyStats.js
+++ count_champ/components/my_stats/MyStats.js
@@ -75,16 +75,16 @@ class MyStats extends React.Component {
 
         let percentOfTotalHandsCorrect = ( (totalHandsCorrect / totalHandsPlayed) * 100 )
         console.log('percent correct: ' + percentOfTotalHandsCorrect)
-        let percentOfTotalHandsCorrectColor = 'blue'
+        let percentOfTotalHandsCorrectColor = '#2196f3'
         if(percentOfTotalHandsCorrect >= 90){
-            console.log('more than 90% right ')
-            percentOfTotalHandsCorrectColor = 'green'
+            //  Good Score
+            percentOfTotalHandsCorrectColor = '#49FF58'
         } else if (percentOfTotalHandsCorrect <= 70){
-            console.log('less than 70% right ')
-            percentOfTotalHandsCorrectColor = 'red'
+            // Bad Score
+            percentOfTotalHandsCorrectColor = 'FF0000'
         } else {
-            console.log('more than 70% less than 90 ')
-            percentOfTotalHandsCorrectColor = 'yellow'
+            //  Medium Score
+            percentOfTotalHandsCorrectColor = '#FBFC5F'
         }
         this.setState({ 
             totalHandsPlayed: totalHandsPlayed, 
@@ -104,13 +104,15 @@ class MyStats extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.statsContainer}>
-                    <Text style={styles.textStyles}>Total Hands</Text>
-                    <Text style={styles.textStyles}>Total Hands Played: {this.state.totalHandsPlayed} </Text>
+                    <Text style={styles.headerStyles}>Total Hands</Text>
+                    <Text style={styles.textStyles}>Played: {this.state.totalHandsPlayed} </Text>
                     <View style={{  width: '100%', 
-                                    height: 30, 
-                                    backgroundColor: 'blue'}}>
+                                    height: 30,
+                                    borderWidth: 1,
+                                    backgroundColor: '#2196f3',
+                                    marginBottom: 5}}>
                     </View>
-                    <Text style={styles.textStyles}>Total Hands Correct: {this.state.totalHandsCorrect} </Text>
+                    <Text style={styles.textStyles}>Correct: {this.state.totalHandsCorrect} </Text>
                     <View style={{  width: `${this.state.percentOfTotalHandsCorrect}%`, 
                                     height: 30, 
                                     backgroundColor: this.state.percentOfTotalHandsCorrectColor}}></View>
@@ -130,12 +132,24 @@ const styles = StyleSheet.create({
     statsContainer: {
         width: ScreenWidth-50,
         marginLeft: 20,
+        backgroundColor: 'black',
+        padding: 20,
+        paddingBottom: 30,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'white',
+    },
+    headerStyles: {
+        color: 'white',
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textDecorationLine: 'underline',
     },
     textStyles: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 16,
         fontWeight: 'bold',
-        textAlign: 'center',
     }
 });
 export default MyStats
