61fe1e1dd38891e6e177a2430e3cae01cb83fce8
Eric-Lingren <ericlingren@gmail.com>
2019-03-07 18:11:53 -0700

    learn bs component built out

--- count_champ/components/learn/level_2_basic_strategy/BasicStrategy.js
+++ count_champ/components/learn/level_2_basic_strategy/BasicStrategy.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 import { Constants } from 'expo';
+import shoe_dh17 from './charts/shoe_dh17.png'
+import shoe_ds17 from './charts/shoe_ds17.png'
+import dd_dh17 from './charts/dd_dh17.png'
+import dd_ds17 from './charts/dd_ds17.png'
+import sd_dh17 from './charts/sd_dh17.png'
+import sd_ds17 from './charts/sd_ds17.png'
 
 let ScreenHeight = Dimensions.get("window").height;
+let ScreenWidth = Dimensions.get("window").width;
 
 class BasicStrategy extends React.Component {
     static navigationOptions = {
@@ -15,33 +22,88 @@ class BasicStrategy extends React.Component {
             <ScrollView>
             <View style={styles.container}>
                 <Text style={styles.paragraph}>
-                Basic strategy is the mathematically correct play for every combination of cards.
-
-                Blackjack is based on mathematical odds
-
-                That’s good! That means there are decisions that can be made on each hand to sway the advantage in your favor.
-
-                There are 312 different play combinations to remember and every one is different. You can practice them all on the training page! Make sure to reference a basic strategy chart. Once you can play basic strategy perfectly on every hand, you will reduce the casino advantage from about 10% to 0.5%.
-
-                In the following situations:
-                Two 8’s - Split
-                Hard 8 or less - Hit
-                10 or 11
-                If higher than the dealer's up card (treating a dealer ace as 11 points) - Double
-                Otherwise - Hit
-                Soft 15 or less - Hit
-                Hard 17 or more - Stand
-                Soft 19 or more - Stand
-                16 against 10 - Surrender
-                Two Aces - Split
-                If your hand doesn’t match any above situations AND the dealer is showing 2 to 6:
-                9 - Double
-                Hard 12 to 16 - Stand
-                Soft 16 to 18 - Double
-                2's, 3's, 6's, 7's, and 9's - Split
-                If your hand doesn’t match any above situations AND the dealer is showing 7 to Ace - Hit
-
+                    Blackjack is based on mathematical odds
                 </Text>
+                <Text style={styles.paragraph}>
+                    That’s good! That means there are decisions that can be made on each hand to sway the advantage in your favor.
+                </Text>
+                <Text style={styles.paragraph}>
+                    Basic strategy is the mathematically correct play for any combination of cards.
+                </Text>
+                <Text style={styles.paragraph}>
+                    There are 312 different play combinations to remember and every one is different. You can practice them all on the training page! Make sure to reference a basic strategy chart. Once you can play basic strategy perfectly on every hand, you will reduce the casino advantage from about 10% to 0.5%.
+                </Text>
+                <Text style={styles.paragraph}>
+                    Here are some general rules of thumb you can use to start memorizing the charts (they may not be accurate for every deck size or rule combination):
+                </Text>
+                <Text style={styles.paragraph}>
+                    Two 8’s - Split
+                </Text>
+                <Text style={styles.paragraph}>
+                    Hard 8 or less - Hit
+                </Text>
+                <Text style={styles.paragraph}>
+                    10 or 11- If higher than the dealer's up card (treating a dealer ace as 11 points) - Double; Otherwise - Hit
+                </Text>
+                <Text style={styles.paragraph}>
+                    Soft 15 or less - Hit
+                </Text>
+                <Text style={styles.paragraph}>
+                    Hard 17 or more - Stand
+                </Text>
+                <Text style={styles.paragraph}>
+                Soft 19 or more - Stand
+                </Text>
+                <Text style={styles.paragraph}>
+                    16 against 10 - Surrender
+                </Text>
+                <Text style={styles.paragraph}>
+                    Two Aces - Split
+                </Text>
+                <Text style={styles.paragraph}>
+                    If your hand doesn’t match any above situations AND the dealer is showing 2 to 6:
+                </Text>
+                <Text style={styles.paragraph}>
+                    9 - Double
+                </Text>
+                <Text style={styles.paragraph}>
+                    Hard 12 to 16 - Stand
+                </Text>
+                <Text style={styles.paragraph}>
+                    Soft 16 to 18 - Double
+                </Text>
+                <Text style={styles.paragraph}>
+                    2's, 3's, 6's, 7's, and 9's - Split
+                </Text>
+                <Text style={styles.paragraph}>
+                    If your hand doesn’t match any above situations AND the dealer is showing 7 to Ace - Hit
+                </Text>
+
+                <Image
+                    style={styles.bsImage}
+                    source={shoe_dh17}
+                />
+                <Image
+                    style={styles.bsImage}
+                    source={shoe_ds17}
+                />
+                <Image
+                    style={styles.bsImage}
+                    source={dd_dh17}
+                />
+                <Image
+                    style={styles.bsImage}
+                    source={dd_ds17}
+                />
+                <Image
+                    style={styles.bsImage}
+                    source={sd_dh17}
+                />
+                <Image
+                    style={styles.bsImage}
+                    source={sd_ds17}
+                />
+
             </View> 
             </ScrollView>
         );
@@ -53,7 +115,7 @@ const styles = StyleSheet.create({
         paddingTop: Constants.statusBarHeight,
         padding: 8,
         backgroundColor: ( '#0f9b0f', '#52c234', '#52c234', '#0f9b0f'),
-        height: ScreenHeight,
+        height: 6200,
     },
     subheader: {
         color: '#fff',
@@ -67,6 +129,13 @@ const styles = StyleSheet.create({
         fontSize: 16,
         padding: 10,
     },
+    bsImage: {
+        width: ScreenWidth - 50,
+        height: 795,
+        marginLeft: 15,
+        marginBottom: 50,
+        borderRadius: 10,
+    }
 });
 
 export default BasicStrategy
